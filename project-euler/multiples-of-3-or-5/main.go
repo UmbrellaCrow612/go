8f9cc549d372ffee1ea3440126e922f23a228dc4
yousaf wazir <umbrellaman612@gmail.com>
2023-05-23 13:38:30 +0100

    revised code

--- project-euler/multiples-of-3-or-5/main.go
+++ project-euler/multiples-of-3-or-5/main.go
@@ -13,21 +13,13 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
 func main() {
-
 	sum := 0
 
 	for i := 1; i < 1000; i++ {
-
-		if i%3 == 0 && i%5 == 0 {
-			sum += i
-		} else if i%3 == 0 && i%5 != 0 {
-			sum += i
-		} else if i%5 == 0 && i%3 != 0 {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
-
 	}
 
 	fmt.Println(sum)
-
 }
